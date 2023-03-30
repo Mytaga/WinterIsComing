409670155126f8a245f676a4b3cdf589d9a111e4
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2023-03-30 19:39:04 +0300

    Add files via upload

---
+++ WinterIsComing.Infrastructure/Migrations/20230330163416_imageUrlAndDescriptionLenghtChanged.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WinterIsComing.Infrastructure.Migrations
+{
+    public partial class imageUrlAndDescriptionLenghtChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Resorts",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Resorts",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Resorts",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Resorts",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+        }
+    }
+}

5babff594a473fd0c7ac23dbc6134abc4efc7bfd
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2023-05-11 20:52:59 +0300

    Add files via upload

---
+++ WinterIsComing.Infrastructure/Data/Models/Error/ErrorDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace WinterIsComing.Core.Models.Error
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; } = null!;
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
